83c315df7141de001b5df01e2615fbaf4fd779c5
Linus Engstam <linus.engstam@gmail.com>
2025-01-02 16:11:51 +0100

    Upload of refined model
    Update includes improved documentation and faster runtime.

---
+++ integrated_p2g/__init__.py
@@ -0,0 +1,17 @@
+# __title__ = "Integrated power-to-gas modeling"
+# __summary__ = "Modeling of a power-to-gas system integrated with co-digestion and wastewater treatment"
+# __url__ = "https://github.com/lengstam/integrated-ptg-model"
+
+# __version__ = "1.0"
+
+# __author__ = "Linus Engstam"
+# __email__ = "linus.engstam@slu.se"
+
+# from . import parameters
+# from . import components
+# from . import other
+# from . import simulation
+# from . import dispatch
+# from . import plotting
+# from . import res_plotting
+# from . import by-products
